9421ea54d7652ff2b8c8eabec5ce2f0c5ab4a7c8
egregory <egregory@tagged.com>
2013-11-23 11:23:32 -0800

    add ui, server for shiny app

---
+++ server.r
@@ -0,0 +1,14 @@
+library(shiny)
+require(rCharts)
+library(tagStats)
+                      
+shinyServer(function(input, output) {
+  output$map <- renderMap({
+   map3 <- Leaflet$new()
+    map3$setView(c(37.8, -96), zoom = 4)
+   map3
+  })
+  output$state <- renderUI({
+    selectInput("state", "Select A State", as.list(gsub(".geo.json", "", list.files(file.path(WGJ_DIR, 'countries', 'USA'), ".geo"), fixed = TRUE)))
+  })
+})
---
+++ ui.r
@@ -0,0 +1,18 @@
+library(shiny)
+require(rCharts)
+library(tagStats)
+
+shinyUI(
+  bootstrapPage(
+    headerPanel("Map Example"),
+    mainPanel(
+#      tags$head(tags$script(src=("js/county.js"))),
+
+      tags$head(tags$script(src=("js/makeMap.js"))),
+      uiOutput("state"),
+      selectInput("getCounties", "Include County Boundaries?", list(TRUE, FALSE)),
+      showOutput("map", "leaflet")
+    )
+
+  )
+)
